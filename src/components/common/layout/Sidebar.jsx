8107263f8c7cec9561d13084b05339d6c4013577
baegyeong <beth0929@naver.com>
2023-07-11 16:12:30 +0900

    fix: change box shadow

--- src/components/common/layout/Sidebar.jsx
+++ src/components/common/layout/Sidebar.jsx
@@ -1,50 +1,50 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 import routes from "../../../routes";
 import MenuList from "./SidebarList";
 import { useNavigate } from "react-router-dom";
-import { GoHomeFill } from 'react-icons/go';
-import { FaPenSquare } from 'react-icons/fa';
-import { HiMiniUserGroup } from 'react-icons/hi2';
-
+import { GoHomeFill } from "react-icons/go";
+import { FaPenSquare } from "react-icons/fa";
+import { HiMiniUserGroup } from "react-icons/hi2";
 
 const Container = styled.div`
-    width : 20rem;
-    height : 45rem;
-    background-color : white;
-    box-shadow:0.2rem 0.3rem 1rem rgba(0, 0, 0, 0.25);
-    position:absolute;
-    top:6rem;
-    left:0;
+  width: 20rem;
+  height: 45rem;
+  background-color: white;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  position: absolute;
+  top: 6rem;
+  left: 0;
 
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    
-    `
-
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function Sidebar() {
-
-
   return (
     <>
       <Container>
-        <MenuList name="Home" icons={<GoHomeFill />} route={routes.home}>
-        </MenuList>
+        <MenuList
+          name="Home"
+          icons={<GoHomeFill />}
+          route={routes.home}
+        ></MenuList>
 
-        <MenuList name="Add Posts" icons={<FaPenSquare />} route={routes.addPost}>
-        </MenuList>
+        <MenuList
+          name="Add Posts"
+          icons={<FaPenSquare />}
+          route={routes.addPost}
+        ></MenuList>
 
-
-        <MenuList name="Mypage" icons={<HiMiniUserGroup />} route={routes.myPage}>
-        </MenuList>
-      </Container >
-
+        <MenuList
+          name="Mypage"
+          icons={<HiMiniUserGroup />}
+          route={routes.myPage}
+        ></MenuList>
+      </Container>
     </>
-
-  )
+  );
 }
 
 export default Sidebar;
