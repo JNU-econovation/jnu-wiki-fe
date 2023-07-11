1a9263bbd6c48a607c1d6d14e34ee37f9c0c8431
baegyeong <beth0929@naver.com>
2023-07-11 16:35:12 +0900

    fix: add login and home route

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import routes from "./routes.js";
 import Home from "./pages/Home.jsx";
-import AddPost from "./pages/AddPost.jsx"
-import MyPage from "./pages/MyPage.jsx"
+import AddPost from "./pages/AddPost.jsx";
+import MyPage from "./pages/MyPage.jsx";
+import Login from "./pages/Login.jsx";
+import Join from "./pages/Join.jsx";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path={routes.home} element={<Home />} />
           <Route path={routes.addPost} element={<AddPost />} />
           <Route path={routes.myPage} element={<MyPage />} />
+          <Route path={routes.login} element={<Login />} />
+          <Route path={routes.join} element={<Join />} />
         </Routes>
       </BrowserRouter>
     </>
