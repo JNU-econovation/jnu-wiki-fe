cf804807446eaaa87b47af5cb5216b9fdf279435
baegyeong <beth0929@naver.com>
2023-07-11 16:34:49 +0900

    feat: create Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/common/layout/Sidebar.jsx";
+import Header from "../components/common/layout/Header.jsx";
+const Login = () => {
+  return (
+    <>
+      <Sidebar></Sidebar>
+      <Header></Header>
+    </>
+  );
+};
+
+export default Login;
