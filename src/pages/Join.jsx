d0cac2ef0f7f524fd432072dbb356fc16fb81681
baegyeong <beth0929@naver.com>
2023-07-11 16:34:28 +0900

    feat: create join page

---
+++ src/pages/Join.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/common/layout/Sidebar.jsx";
+import Header from "../components/common/layout/Header.jsx";
+const Join = () => {
+  return (
+    <>
+      <Sidebar></Sidebar>
+      <Header></Header>
+    </>
+  );
+};
+
+export default Join;
