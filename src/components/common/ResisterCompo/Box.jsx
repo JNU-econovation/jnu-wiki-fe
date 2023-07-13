37e5479fd4205e29c5795b69ab0617b03344061b
GimGayeon <kky1373@naver.com>
2023-07-13 17:36:33 +0900

    feat:Login-components

---
+++ src/components/common/ResisterCompo/Box.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const BoxCss = styled.div`
+    background-color: #ffffff;
+
+    width: 423px;
+
+    display: flex;
+    flex-direction: column;
+
+    position: relative;
+    bottom:  ${(props) => props ? '0' : '2rem'};;
+
+
+`
+
+
+const Box = ({ children, bottom }) => {
+    return (
+
+        <BoxCss bottom={bottom}>{children}</BoxCss>
+    )
+}
+
+export default Box;
---
+++ src/components/common/ResisterCompo/Label.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const LabelCss = styled.label`
+    text-align : start;
+    color: #216D32; 
+
+    margin: 0.8rem 0;
+`
+
+const Label = ({ htmlFor, child }) => {
+    return (
+        <LabelCss htmlFor={htmlFor}>{child}</ LabelCss>
+    )
+}
+
+export default Label;
