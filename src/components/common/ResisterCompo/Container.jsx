06e7859686af0f9e496471c5b4827c47db02cd6d
baegyeong <beth0929@naver.com>
2023-07-13 19:45:31 +0900

    fix: set position fixed

--- src/components/common/ResisterCompo/Container.jsx
+++ src/components/common/ResisterCompo/Container.jsx
@@ -1,38 +1,27 @@
 import styled from "styled-components";
 
-
-
-
-
-const Container = ({ children }) => {
-    return (
-
-        <ContainerCss>{children}</ContainerCss>
-    )
-}
-
+const Container = ({ children, marginBottom }) => {
+  return <ContainerCss marginBottom={marginBottom}>{children}</ContainerCss>;
+};
 
 const ContainerCss = styled.div`
-    background-color: #ffffff;
-    border: 0.5px solid #878787;
-    border-radius: 0.5rem;
-    //밖그림자
-    box-shadow: 0px 4px 64px rgba(0, 0, 0, 0.05);
+  background-color: #ffffff;
+  border: 0.5px solid #878787;
+  border-radius: 0.5rem;
+  //밖그림자
+  box-shadow: 0px 4px 64px rgba(0, 0, 0, 0.05);
 
-    width: 26rem;
-    padding:0 3rem 0 3rem;
+  width: 26rem;
+  padding: 0 3rem 0 3rem;
 
-    display: flex;
-    justify-content: end;
-    flex-direction: column;
+  display: flex;
+  justify-content: end;
+  flex-direction: column;
 
-    position: relative;
-    left: 30rem;
-    top:3rem;
-
-
-`
-
+  position: relative;
+  left: 35rem;
+  top: 15rem;
+  z-index: 1;
+`;
 
 export default Container;
-
