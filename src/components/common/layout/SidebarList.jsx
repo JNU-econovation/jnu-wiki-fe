4283f297abeb1c950e3add75839a36fc095e841b
baegyeong <beth0929@naver.com>
2023-07-17 01:00:59 +0900

    fix: each sidebar change cursor

--- src/components/common/layout/SidebarList.jsx
+++ src/components/common/layout/SidebarList.jsx
@@ -1,57 +1,57 @@
 import styled from "styled-components";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import routes from "../../../routes";
 
 const MenuStyle = styled.div`
-    display:flex;
-    align-items: center;
-    list-style-type: none;
-    background-color : white;
-    color:rgba(95, 150, 107, 1);
+  display: flex;
+  align-items: center;
+  list-style-type: none;
+  background-color: white;
+  color: rgba(95, 150, 107, 1);
 
-    width : 17rem;
-    margin :0.5rem;
-    height:3rem;
-    border-radius:0.2rem;
+  width: 17rem;
+  margin: 0.5rem;
+  height: 3rem;
+  border-radius: 0.2rem;
+  font-size: 1rem;
+
+  cursor: pointer;
+
+  &:first-child {
+    margin-top: 2rem;
+  }
+
+  &:hover {
+    background-color: rgba(222, 233, 224, 1);
+    color: rgba(33, 109, 50, 1);
     font-size: 1rem;
-
-
-    &:first-child {
-        margin-top:2rem;
-    }
-
-
-    &:hover{  
-        background-color : rgba(222, 233, 224, 1);
-        color :rgba(33, 109, 50, 1);
-        font-size:1rem;
-        font-weight: 600;
-      }
-
-    `
+    font-weight: 600;
+  }
+`;
 const MenuIcon = styled.div`
-    font-size: 2rem;
-    padding:0 1rem;
-`
-
+  font-size: 2rem;
+  padding: 0 1rem;
+`;
 
 const MenuList = ({ name, icons, route }) => {
-    const navigate = useNavigate();
-    return (
-        <>
-            <MenuStyle onClick={() => { navigate(route); }}>
-                <MenuIcon>{icons}</MenuIcon>
-                {name}
-            </MenuStyle>
-        </>
-    );
+  const navigate = useNavigate();
+  return (
+    <>
+      <MenuStyle
+        onClick={() => {
+          navigate(route);
+        }}
+      >
+        <MenuIcon>{icons}</MenuIcon>
+        {name}
+      </MenuStyle>
+    </>
+  );
 };
 
 MenuList.propTypes = {
-    name: PropTypes.string,
-
-
-}
+  name: PropTypes.string,
+};
 
 export default MenuList;
