cd4d05492870777183b2674e3e017b74566f55cf
baegyeong <beth0929@naver.com>
2023-07-20 16:05:42 +0900

    fix: fix font styles

--- src/components/common/Document/HelperMsg.jsx
+++ src/components/common/Document/HelperMsg.jsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 const HelperMsg = styled.p`
+  margin-top: 0.5rem;
+
   color: #6b6b6b;
+  font-size: 0.8rem;
+  font-weight: 400;
 `;
 
 const DocumentInput = ({ children }) => {
