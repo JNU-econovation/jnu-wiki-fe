fd46160b1164d2361f1f24cb235730bea2df994f
baegyeong <beth0929@naver.com>
2023-07-22 17:44:45 +0900

    feat: create docs validation hook

---
+++ src/hooks/useValidation.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const useValidation = (initialValue) => {
+  const [msg, setMsg] = useState(initialValue);
+
+  const docsNameCheck = (docsName) => {
+    return docsName === "" ? "문서 제목을 입력해주세요." : "";
+  };
+  const docsLocation = (docsLocation) => {
+    return docsLocation === "" ? "위치를 지도에서 클릭해주세요" : "";
+  };
+  const docsCategory = (docsCategory) => {
+    return docsCategory === "" ? "카테고리를 입력해주세요." : "";
+  };
+
+  const handleSetMsg = (e) => {
+    const { value, id } = e.target;
+    let newMsg = "";
+    if (id === "docsName") {
+      newMsg = docsNameCheck(value);
+    } else if (id === "docsLocation") {
+      newMsg = docsLocation(value);
+    } else {
+      newMsg = docsCategory(value);
+    }
+    setMsg(newMsg);
+  };
+
+  return { msg, handleSetMsg };
+};
+
+export default useValidation;
