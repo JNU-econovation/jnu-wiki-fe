18f2a3bc8f0df2032f474cfbb86a94508bd14b24
baegyeong <beth0929@naver.com>
2023-07-22 17:42:50 +0900

    feat: show address name

--- src/components/common/layout/Map.jsx
+++ src/components/common/layout/Map.jsx
@@ -8,11 +8,12 @@ const MapDiv = styled.div`
   top: 6rem;
   z-index: -1;
 `;
+
 const Map = () => {
   const markerRef = useRef(null);
   useEffect(() => {
     mapscript();
-  });
+  }, []);
 
   const mapscript = () => {
     const container = document.getElementById("map");
@@ -24,26 +25,68 @@ const Map = () => {
 
     // let markerPosition = new kakao.maps.LatLng(35.175636, 126.907136); // center
 
-    let marker = new kakao.maps.Marker({
-      position: map.getCenter(),
-    });
+    function searchAddFromCoords(coords, callback) {
+      // 좌표로 행정동 주소 정보를 요청합니다
+      geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
+    }
 
-    marker.setMap(map);
+    function searchDetailAddrFromCoords(coords, callback) {
+      // 좌표로 법정동 상세 주소 정보를 요청합니다
+      geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+    }
+
+    let geocoder = new kakao.maps.services.Geocoder();
+
+    let marker = new kakao.maps.Marker(),
+      infowindow = new kakao.maps.InfoWindow({ zindex: 1 });
+
+    searchAddFromCoords(map.getCenter());
+
     kakao.maps.event.addListener(map, "click", function (mouseEvent) {
-      let latlng = mouseEvent.latLng;
-      marker.setPosition(latlng);
+      searchDetailAddrFromCoords(mouseEvent.latLng, function (result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+          console.log(result);
+          let detailAddr = !result[0].road_address
+            ? "<div>도로명주소 : " +
+              result[0].road_address.address_name +
+              "</div>"
+            : "";
+          detailAddr +=
+            "<div>지번 주소 : " + result[0].address.address_name + "</div>";
+
+          let content =
+            '<div class="bAddr">' +
+            '<span class="title">법정동 주소정보</span>' +
+            detailAddr +
+            "</div>";
+
+          // 마커를 클릭한 위치에 표시합니다
+          marker.setPosition(mouseEvent.latLng);
+          marker.setMap(map);
+
+          // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
+          infowindow.setContent(content);
+          infowindow.open(map, marker);
+        }
+      });
+    });
+    // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
+    kakao.maps.event.addListener(map, "idle", function () {
+      searchAddFromCoords(map.getCenter());
     });
   };
 
   return (
-    <MapDiv
-      id="map"
-      ref={markerRef}
-      style={{
-        width: "80%",
-        height: "100vh",
-      }}
-    />
+    <>
+      <MapDiv
+        id="map"
+        ref={markerRef}
+        style={{
+          width: "80%",
+          height: "100vh",
+        }}
+      />
+    </>
   );
 };
 
