6fb98d0866be9b3ec5c77fc28847949c00ba2980
baegyeong <beth0929@naver.com>
2023-07-23 01:37:15 +0900

    feat: add category reducer at rootReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore } from "redux";
 import latLngReducer from "./latLngReducer";
 import addressReducer from "./addressReducer";
+import categoryReducer from "./categoryReducer";
 
 const rootReducer = combineReducers({
   latLng: latLngReducer,
   address: addressReducer,
+  category: categoryReducer,
 });
 
 const store = createStore(rootReducer);
