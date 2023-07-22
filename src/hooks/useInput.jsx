e1e8ec8e412023b2db650723ae2a8a2ee55e52a0
baegyeong <beth0929@naver.com>
2023-07-23 00:46:11 +0900

    chore: install redux

--- src/hooks/useInput.jsx
+++ src/hooks/useInput.jsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from "react";
 
-
 const useInput = (initialValue) => {
-    const [valueInit, setValue] = useState(initialValue);
-    const handleOnChange = (e) => {
-        const { id, value } = e.target;
-        setValue({ ...valueInit, [id]: value });
-    }
-    useEffect((e) => {
-        // console.log(valueInit.password);
-    }, [valueInit]);
-    return { valueInit, handleOnChange };
+  const [valueInit, setValue] = useState(initialValue);
+  const handleOnChange = (e) => {
+    const { id, value } = e.target;
+    setValue({ ...valueInit, [id]: value });
+  };
+  useEffect(
+    (e) => {
+      // console.log(valueInit.password);
+    },
+    [valueInit]
+  );
+  return { valueInit, handleOnChange };
 };
 
 export default useInput;
