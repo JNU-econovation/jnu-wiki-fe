353a0b380f6733f223f5caee48e9d583bd426a67
GimGayeon <kky1373@naver.com>
2023-07-24 00:59:49 +0900

    fix:Add header-logout

--- src/components/common/layout/Header.jsx
+++ src/components/common/layout/Header.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import "/public/fonts/pretendard.css";
 import jnuLogo from "/public/jnu-logo.png";
-import { useRef } from "react";
+import { useEffect, useRef,useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 import routes from "../../../routes";
+import Swal from "sweetalert2";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -18,6 +19,12 @@ const Header = () => {
     focusRef.current.placeholder = "      검색";
   };
 
+  const [JWT, setJWT] = useState(null)
+    useEffect(() => {
+        setJWT(localStorage.getItem('token'));
+    }, [JWT]);
+
+    console.log(JWT)
   return (
     <>
       <Container>
@@ -39,24 +46,52 @@ const Header = () => {
             onFocus={onFocusSearchBar}
             onBlur={onBlurSearchBar}
           />
-          <Button
-            type="click"
-            color="primary"
-            border="1px solid #216D32"
-            backgroundcolor="white"
-            onClick={() => navigate(routes.join)}
-          >
-            회원가입
-          </Button>
-          <Button
-            type="click"
-            color="white"
-            border="none"
-            backgroundcolor="primary"
-            onClick={() => navigate(routes.login)}
-          >
-            로그인
-          </Button>
+         {!JWT ?
+         (
+          <> <Button
+          type="click"
+          color="primary"
+          border="1px solid #216D32"
+          backgroundcolor="white"
+          onClick={() => navigate(routes.join)}
+        >
+          회원가입
+        </Button>
+        <Button
+          type="click"
+          color="white"
+          border="none"
+          backgroundcolor="primary"
+          onClick={() => navigate(routes.login)}
+        >
+          로그인
+        </Button></>
+         )
+         :<Button
+         type="click"
+         color="white"
+         border="none"
+         backgroundcolor="primary"
+         onClick={() => {
+          Swal.fire({
+            icon: 'info',
+            title: '로그아웃 하시겠습니까?',
+            showCancelButton: true,
+            confirmButtonText: '예', 
+            cancelButtonText: '아니오',
+            confirmButtonColor: '#429f50',
+            cancelButtonColor: '#d33',
+          }).then(result => {
+            if (result.isConfirmed) {
+              localStorage.removeItem('token')
+              setJWT(null);
+            }
+        })
+          
+         }}
+       >
+         로그아웃
+       </Button>}
         </HeaderDiv>
         <Line />
       </Container>
