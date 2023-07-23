a22a648214178e4f2699f85f299266b84e259645
GimGayeon <kky1373@naver.com>
2023-07-23 13:25:45 +0900

    fix:Add mypageTestData

--- src/pages/BasicInfoEditReq.jsx
+++ src/pages/BasicInfoEditReq.jsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router-dom";
+import MainLayout from "../components/common/layout/MainLayout";
 const BasicInfoEditReq = () => {
     const { id } = useParams()
     return (
-        <div>
+        <>
+        <MainLayout>
             이 페이지의 아이디는 {id} 입니다.
-        </div>
+        </MainLayout></>
+        
     );
 };
 
