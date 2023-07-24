5af54cf5f225e5b30de03bfdc64011f8294d42f2
baegyeong <beth0929@naver.com>
2023-07-25 02:43:36 +0900

    chore: add testData

---
+++ src/components/common/document/testData.js
@@ -0,0 +1,17 @@
+export const testData = [
+  {
+    success: true,
+    response: [
+      {
+        id: 1,
+        docsName: "팬도로시",
+        docsCategory: "CAFE",
+        docsLocation: { lat: "33.45048921971038", lng: "126.56945764814947" },
+        docsContent: "나 쿠키 내가 만든 쿠키 역시 쿠키는 초코 쿠키",
+        docsCreatedBy: "cookie",
+        docsCreatedAt: "2024-09-09T10:00",
+      },
+    ],
+    error: null,
+  },
+];
