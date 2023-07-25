8611813798a38b94ca82750ecf021dd9f0b0e25e
baegyeong <beth0929@naver.com>
2023-07-26 00:48:25 +0900

    fix: modify css

--- src/components/common/document/DocumentLabel.jsx
+++ src/components/common/document/DocumentLabel.jsx
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 
+const SpanWidth = styled.div`
+  display: inline-block;
+  width: 4rem;
+`;
 const LabelStyled = styled.label`
   color: #216d32;
 `;
 
 const DocumentLabel = ({ htmlFor, children }) => {
-  return <LabelStyled htmlFor={htmlFor}>{children}</LabelStyled>;
+  return (
+    <SpanWidth>
+      <LabelStyled htmlFor={htmlFor}>{children}</LabelStyled>
+    </SpanWidth>
+  );
 };
 
 export default DocumentLabel;
