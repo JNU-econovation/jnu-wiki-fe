3723f8098ef3fc773ba1b043f79b1a4881b4c6e7
baegyeong <beth0929@naver.com>
2023-07-25 10:08:39 +0900

    feat: feature #27 finish

--- src/components/common/document/testData.js
+++ src/components/common/document/testData.js
@@ -6,7 +6,7 @@ export const testData = [
         id: 1,
         docsName: "팬도로시",
         docsCategory: "CAFE",
-        docsLocation: { lat: "33.45048921971038", lng: "126.56945764814947" },
+        docsLocation: { lat: "35.1784", lng: "126.9097" },
         docsContent: "나 쿠키 내가 만든 쿠키 역시 쿠키는 초코 쿠키",
         docsCreatedBy: "cookie",
         docsCreatedAt: "2024-09-09T10:00",
