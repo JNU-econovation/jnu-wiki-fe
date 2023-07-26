e32c4ed7a423166c54212ad06653db1feab66850
GimGayeon <kky1373@naver.com>
2023-07-26 14:39:28 +0900

    merge

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: { exclude: ["fsevents"] },
+ optimizeDeps: { exclude: ["fsevents"] },
+ //DEBUG=vite:*
 })
