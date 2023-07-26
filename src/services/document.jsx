a88d3df4d73b61a3f4b8bc0ab0fcd6a5130c162b
baegyeong <beth0929@naver.com>
2023-07-26 09:07:04 +0900

    fix: set mock server

--- src/services/document.jsx
+++ src/services/document.jsx
@@ -16,7 +16,15 @@ export const detailDocument = (id = 0) => {
 
   return instance.get(
     // "/docs/" + id
-    "https://a1384822-b40b-4a9e-ae44-598dc20b6ad1.mock.pstmn.io//docs/%7Bdocs_id%7D"
+    // "https://a1384822-b40b-4a9e-ae44-598dc20b6ad1.mock.pstmn.io/docs/"
+    "https://a1384822-b40b-4a9e-ae44-598dc20b6ad1.mock.pstmn.io/docs?id=1"
+  );
+};
+
+export const docsList = () => {
+  return instance.get(
+    // "/docs/"
+    "https://a1384822-b40b-4a9e-ae44-598dc20b6ad1.mock.pstmn.io/docs"
   );
 };
 
