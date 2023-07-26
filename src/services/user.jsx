37e68e146552e55c00b013bee1d1255e84ad04c4
GimGayeon <kky1373@naver.com>
2023-07-27 02:38:38 +0900

    api test

--- src/services/user.jsx
+++ src/services/user.jsx
@@ -11,7 +11,7 @@ export const login = async (data) => {
 export const nicknameDoubleCheck = (nickname) => {
     return instance.post('https://f7cf390b-1657-42d2-a51f-30739cae9efe.mock.pstmn.io/members/check/nickname', { 'nickname':nickname });
 }
-export const emailDoubleCheck = (email) => {
+export const emailDBCheck = (email) => {
     return instance.post('/members/check/email', { email });
 }
 
@@ -37,13 +37,37 @@ export const getChangeInfo = (data) => {
     return instance.patch('https://f7cf390b-1657-42d2-a51f-30739cae9efe.mock.pstmn.io/members/modify/change', { nickname, password });
 }
 
-///admin
+//admin List
 export const basicInfoEditRequest =()=>{
-    return instance.get('/admin/requests/update')
+    return instance.get('https://f7cf390b-1657-42d2-a51f-30739cae9efe.mock.pstmn.io/admin/requests/update')
 }
 export const newInfoCreateRequest =()=>{
-    return instance.get('/requests/new')
+    return instance.get('https://f7cf390b-1657-42d2-a51f-30739cae9efe.mock.pstmn.io/requests/new')
+}
+
+//admin detail
+export const newDocsRequest =(docs_request_id)=>{
+    return instance.get(`/requests/new/${docs_request_id}`)
+}
+export const editDocsRequest =(docs_request_id)=>{
+    return instance.get(`/admin/update/${docs_request_id}`)
+}
+
+export const docsRequest =(docs_id)=>{
+    return instance.get(`/docs/${docs_id}`)
 }
 
 
+//admin request 
+export const newRequestApprove =(docs_request_id)=>{
+    return instance.post(`/requests/new/${docs_request_id}/approve`)
+}
+
+export const requestReject =(docs_request_type,docs_request_id)=>{
+    return instance.post(`/admin/${docs_request_type}/${docs_request_id}/reject`)
+}
+
+export const editRequestApprove =(docs_request_id)=>{
+    return instance.patch(`/admin/update/${docs_request_id}/approve`)
+}
 
