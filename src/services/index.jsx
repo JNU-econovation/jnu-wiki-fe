3d2ead4223ab61af8148ea68b1263db2e1f68068
baegyeong <beth0929@naver.com>
2023-07-28 05:31:28 +0900

    fix: add toastify libraries

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,35 +1,33 @@
 import axios from "axios";
 import routes from "../routes";
 
-
 export const instance = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_URL,
+  baseURL: import.meta.VITE_APP_API_URL,
   timeout: 1000 * 5,
   headers: {
     "Content-Type": "application/json",
-    
   },
 });
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers["Authorization"] = `${token}`;
+    config.headers["Authorization"] = `Bearer ${token}`;
   }
   return config;
 });
 
-// instance.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   (error) => {
-//     if (error.response.status === 401) {
-//       localStorage.removeItem("token");
-//       window.location.href = `${routes.home}`;
-//       return Promise.resolve(error.response.data.error.message);
-//     }
-//     return Promise.reject(error.response);
-//   }
-// );
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = `${routes.home}`;
+      return Promise.resolve(error.response.data.error.message);
+    }
+    return Promise.reject(error.response);
+  }
+);
 //401 에러 캐치(jwt 만료)
