e9589998a4e240866a82a9fd6114465aeb8036d2
baegyeong <beth0929@naver.com>
2023-07-27 23:17:05 +0900

    fix: add unstaged file

--- src/components/common/document/DocumentHeading.jsx
+++ src/components/common/document/DocumentHeading.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
-import EditPhoto from "./EditPhoto";
 import { PiPencilLineThin } from "react-icons/Pi";
+import { TfiSave } from "react-icons/Tfi";
+import { MdOutlineCancel } from "react-icons/Md";
 
 const Group = styled.span`
   display: flex;
   align-items: center;
+  color: #216d32;
 `;
 const StyledHeading = styled.p`
   font-size: 1.4rem;
@@ -17,16 +19,56 @@ const StyledHeading = styled.p`
 
 const StyledPencil = styled(PiPencilLineThin)`
   cursor: pointer;
-  color: #216d32;
-  font-size: 1.2rem;
+  font-size: 1.3rem;
+  margin: 0 0.5rem;
 `;
 
-const DocumentHeading = ({ children, onClick }) => {
+const StyledSave = styled(TfiSave)`
+  cursor: pointer;
+  margin: 0 0.5rem;
+`;
+
+const StyledCancel = styled(MdOutlineCancel)`
+  cursor: pointer;
+  font-size: 1.3rem;
+`;
+
+const DocumentHeading = ({
+  children,
+  onClick,
+  type = false,
+  contentType = false,
+  save = false,
+  cancel = false,
+  className,
+}) => {
   return (
     <>
       <Group>
         <StyledHeading>{children}</StyledHeading>
-        <StyledPencil onClick={onClick} />
+        {className === "basic" ? (
+          <>
+            {type === false ? (
+              <StyledPencil onClick={onClick} />
+            ) : (
+              <>
+                <StyledSave className="save" onClick={onClick} />
+                <StyledCancel className="cancel" onClick={onClick} />
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            {contentType === false ? (
+              <StyledPencil onClick={onClick} />
+            ) : (
+              <>
+                <StyledSave className="save" onClick={onClick} />
+                <StyledCancel className="cancel" onClick={onClick} />
+              </>
+            )}
+          </>
+        )}
       </Group>
     </>
   );
