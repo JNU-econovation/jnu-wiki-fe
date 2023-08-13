52e759f61876da7d897353c79a97eb4b9c87c239
baegyeong <beth0929@naver.com>
2023-08-14 01:38:47 +0900

    feat: show CreateDocument component based on click action
    add animation effect

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,14 +1,37 @@
-import MainLayout from "../components/common/layout/MainLayout";
+import Sidebar from "../components/common/layout/Sidebar";
+import Header from "../components/common/layout/Header";
 import Map from "../components/common/layout/Map";
 import CreateDocument from "../components/common/document/CreateDocument";
+import { useState } from "react";
+import styled from "styled-components";
+
+const DocumentWrapper = styled.div`
+  -webkit-animation: fade-in 1s;
+
+  @-webkit-keyframes fade-in {
+    0% {
+      opacity: 0;
+      transform: translate3d(-100%, 0, 0);
+    }
+    to {
+      opacity: 1;
+      transform: translateZ(0);
+    }
+  }
+`;
 
 const AddPost = () => {
+  const [show, setShow] = useState(true);
+
+  const handleShow = () => {
+    setShow(!show);
+  };
   return (
     <>
-      <MainLayout>
-        <CreateDocument />
-        <Map />
-      </MainLayout>
+      <Sidebar onClick={handleShow} />
+      <Header />
+      <DocumentWrapper>{show ? <CreateDocument /> : undefined}</DocumentWrapper>
+      <Map />
     </>
   );
 };
