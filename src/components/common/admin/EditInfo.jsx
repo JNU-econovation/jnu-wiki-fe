43a6ac0d28cc40e58c584fc55b6d80b70664809e
GimGayeon <kky1373@naver.com>
2023-08-14 00:53:51 +0900

    fix:관리자페이지 Error

--- src/components/common/admin/EditInfo.jsx
+++ src/components/common/admin/EditInfo.jsx
@@ -1,69 +1,74 @@
-
 import styled from "styled-components";
-import { useState ,useEffect} from "react";
+import { useState, useEffect } from "react";
 
 const TextTitleP = styled.p`
   font-weight: 600;
   font-size: 15px;
-  color:#216D32;
+  color: #216d32;
   width: 5rem;
 
- margin-bottom: 1.5rem;
+  margin-bottom: 1.5rem;
 `;
 const TextP = styled.p`
-font-weight: 200;
-font-size: 15px;
-color:#000000;
+  font-weight: 200;
+  font-size: 15px;
+  color: #000000;
 
-margin-bottom: 1.5rem;
-text-decoration: ${({textDecoration}) => (textDecoration? 'line-through' : null)};
+  margin-bottom: 1.5rem;
+  text-decoration: ${({ textDecoration }) =>
+    textDecoration ? "line-through" : null};
 `;
 const ModifyP = styled.p`
-font-weight: 200;
-font-size: 15px;
-color:#ff0000;
+  font-weight: 200;
+  font-size: 15px;
+  color: #ff0000;
 
-margin-bottom: 1.7rem;
+  margin-bottom: 1.7rem;
 `;
 const EditBox = styled.div`
  display: flex;
  width
 `;
 
+const EditInfo = ({ children, child, modify, address, textDecoration }) => {
+  //정보들 다 가져와서 만약에 정보가 다르면 modify 활성
+  //if child!= modify => modify,textdecorations 라는 usestate true
 
-const EditInfo = ({children,child,modify,address,textDecoration}) => {
-//정보들 다 가져와서 만약에 정보가 다르면 modify 활성
-//if child!= modify => modify,textdecorations 라는 usestate true 
-    console.log(address)
-    console.log(modify)
-    return (
-        <EditBox>
-            {children?
-                 <TextTitleP>{children}</TextTitleP>
-                 :null
+  return (
+    <EditBox>
+      {children ? <TextTitleP>{children}</TextTitleP> : null}
+
+      <div>
+        {child ? (
+          <TextP
+            textDecoration={
+              modify && modify != child && modify != address
+                ? textDecoration
+                : false
             }
-           
-            <div>
-                {child?
-               <TextP textDecoration={
-                modify&&modify!=child&&modify!=address ?
-                textDecoration:false
-            }>{child}</TextP>
-                :null}
-                
-                {address?
-                <TextP textDecoration={
-                    modify&&modify!=child&&modify!=address ?
-                    textDecoration:false}>{address}</TextP>
-                :null}
+          >
+            {child}
+          </TextP>
+        ) : null}
 
-                {modify&&modify!=child&&modify!=address ?
-                    <ModifyP>{modify}</ModifyP>
-                    :
-                    null}
-            </div>
-        </EditBox>
-    );
+        {address ? (
+          <TextP
+            textDecoration={
+              modify && modify != child && modify != address
+                ? textDecoration
+                : false
+            }
+          >
+            {address}
+          </TextP>
+        ) : null}
+
+        {modify && modify != child && modify != address ? (
+          <ModifyP>{modify}</ModifyP>
+        ) : null}
+      </div>
+    </EditBox>
+  );
 };
 
 export default EditInfo;
--- src/services/user.jsx
+++ src/services/user.jsx
@@ -1,73 +1,71 @@
 import { instance } from "./index";
 
 export const login = async (data) => {
-    const { email, password } = data;
-    return await instance.post('members/login', {
-        email,
-        password,
-    })
-}
+  const { email, password } = data;
+  return await instance.post("members/login", {
+    email,
+    password,
+  });
+};
 
 export const nicknameDoubleCheck = (name) => {
-    return instance.post('members/check/nickname', { 'nickname':name });
-}
+  return instance.post("members/check/nickname", { nickname: name });
+};
 export const emailDBCheck = (email) => {
-    return instance.post('members/check/email', { email });
-}
+  return instance.post("members/check/email", { email });
+};
 
 export const register = (data) => {
-    const { email, password, nickName } = data;
-    return instance.post('members/join', {
-        email,
-        password,
-        nickName
-    })
-}
-
-
-
+  const { email, password, nickName } = data;
+  return instance.post("members/join", {
+    email,
+    password,
+    nickName,
+  });
+};
 
 ///mypage
 export const getUserInfo = () => {
-    return instance.get('members/info');
-}
+  return instance.get("members/info");
+};
 
 export const getChangeInfo = (data) => {
-    const { Newnickname, Newpassword } = data;
-    return instance.post('members/modify/change', { nickname:Newnickname, password: Newpassword });
-}
+  const { Newnickname, Newpassword } = data;
+  return instance.post("members/modify/change", {
+    nickname: Newnickname,
+    password: Newpassword,
+  });
+};
 
 //admin List
-export const basicInfoEditRequest =()=>{
-    return instance.get('admin/requests/update')
-}
-export const newInfoCreateRequest =()=>{
-    return instance.get('admin/requests/new')
-}
+export const basicInfoEditRequest = () => {
+  return instance.get("admin/requests/update");
+};
+export const newInfoCreateRequest = () => {
+  return instance.get("admin/requests/new");
+};
 
 //admin detail
-export const newDocsRequest =(docs_request_id)=>{
-    return instance.get(`admin/requests/new/${docs_request_id}`)
-}
-export const editDocsRequest =(docs_request_id)=>{
-    return instance.get(`admin/requests/update/${docs_request_id}`)
-}
+export const newDocsRequest = (docs_request_id) => {
+  return instance.get(`admin/requests/new/${docs_request_id}`);
+};
+export const editDocsRequest = (docs_request_id) => {
+  return instance.get(`admin/requests/update/${docs_request_id}`);
+};
 
-export const docsRequest =(docs_id)=>{
-    return instance.get(`docs/${docs_id}`)
-}
+export const docsRequest = (docs_id) => {
+  return instance.get(`docs/${docs_id}`);
+};
 
+//admin request
+export const newRequestApprove = async (docsRequestId) => {
+  return await instance.post(`admin/approve/new/${docsRequestId}`);
+};
 
-//admin request 
-export const newRequestApprove =async(docsRequestId)=>{
-    return await instance.post(`admin/approve/new/${docsRequestId}`)
-}
+export const requestReject = async (docsRequestId) => {
+  return await instance.post(`admin/reject/${docsRequestId}`);
+};
 
-export const requestReject =async(docsRequestId)=>{
-    return await instance.post(`admin/reject/${docsRequestId}`)
-}
-
-export const editRequestApprove =async(docsRequestId)=>{
-    return await instance.post(`admin/approve/update/${docsRequestId}`)
-}
-
+export const editRequestApprove = async (docsRequestId) => {
+  return await instance.post(`admin/approve/update/${docsRequestId}`);
+};
