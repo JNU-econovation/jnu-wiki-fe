1e641e1d23169441e2a98a020263e29951e109c1
baegyeong <beth0929@naver.com>
2023-08-14 01:46:38 +0900

    fix: create DocumentWrapper component

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,23 +1,8 @@
 import MainLayout from "../components/common/layout/MainLayout";
 import Map from "../components/common/layout/Map";
 import CreateDocument from "../components/common/document/CreateDocument";
+import DocumentWrapper from "../components/common/document/DocumentWrapper";
 import { useState } from "react";
-import styled from "styled-components";
-
-const DocumentWrapper = styled.div`
-  -webkit-animation: fade-in 1s;
-
-  @-webkit-keyframes fade-in {
-    0% {
-      opacity: 0;
-      transform: translate3d(-100%, 0, 0);
-    }
-    to {
-      opacity: 1;
-      transform: translateZ(0);
-    }
-  }
-`;
 
 const AddPost = () => {
   const [show, setShow] = useState(true);
