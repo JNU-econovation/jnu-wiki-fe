ab4c07427e70f3eb77123dcded052a68626d579e
baegyeong <beth0929@naver.com>
2023-08-14 01:44:14 +0900

    feat: add onClick prop

--- src/components/common/layout/MainLayout.jsx
+++ src/components/common/layout/MainLayout.jsx
@@ -1,10 +1,10 @@
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 
-const MainLayout = ({ active, children }) => {
+const MainLayout = ({ active, children, onClick }) => {
   return (
     <>
-      <Sidebar active={active} />
+      <Sidebar active={active} onClick={onClick} />
       <Header />
       {children}
     </>
