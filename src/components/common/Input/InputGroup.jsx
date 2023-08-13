6b8730e6bd4c3c557c6247655a42f13fc7005613
GimGayeon <kky1373@naver.com>
2023-08-13 15:44:00 +0900

    prettier

--- src/components/common/Input/InputGroup.jsx
+++ src/components/common/Input/InputGroup.jsx
@@ -1,20 +1,35 @@
 import Input from "./Input";
 import Box from "../Resister/Box";
-import Label from "../Resister/Label"
+import Label from "../Resister/Label";
 import AlertMessage from "../Resister/AlertMessage";
 import DoubleCheck from "../Resister/DoubleCheck";
 
-
-const InputGroup = ({ id, name, type, value, onChange, label, placeholder, para, doublecheck, margin }) => {
-
-    return (
-        <Box>
-            <Label htmlFor={id} child={label} margin={margin}></Label>
-            <Input id={id} type={type} name={name} value={value} onChange={onChange} placeholder={placeholder} />
-            <AlertMessage para={para}></AlertMessage>
-        </Box>
-    )
-}
-
+const InputGroup = ({
+  id,
+  name,
+  type,
+  value,
+  onChange,
+  label,
+  placeholder,
+  para,
+  doublecheck,
+  margin,
+}) => {
+  return (
+    <Box>
+      <Label htmlFor={id} child={label} margin={margin}></Label>
+      <Input
+        id={id}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+      <AlertMessage para={para}></AlertMessage>
+    </Box>
+  );
+};
 
 export default InputGroup;
