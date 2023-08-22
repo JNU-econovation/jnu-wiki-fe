8fbda57e5b5830d8fc31e376b7a1e62e161be63b
baegyeong <beth0929@naver.com>
2023-08-23 01:58:58 +0900

    feat: add lat, lng prop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import MainLayout from "../components/common/layout/MainLayout";
 import Map from "../components/common/layout/Map";
+import { useQuery } from "@tanstack/react-query";
+import { mapDocument } from "../services/document";
 
 const Home = () => {
+  const { data, isLoading, isError } = useQuery(["valid_pin"], mapDocument);
+
+  const latitude = data?.data?.response.map((x) => x.docsLocation.lat);
+  const longitude = data?.data?.response.map((x) => x.docsLocation.lng);
+
   return (
     <>
       <MainLayout>
-        <Map />
+        {!isLoading && !isError && <Map apiLat={latitude} apiLng={longitude} />}
       </MainLayout>
     </>
   );
