b39ef2f2cce961bd4b7629178301ae9547649ca9
baegyeong <beth0929@naver.com>
2023-09-12 17:51:16 +0900

    fix: 아이콘 순서 변경

--- src/components/common/document/ToggleBtn.jsx
+++ src/components/common/document/ToggleBtn.jsx
@@ -1,13 +1,13 @@
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 import styled from "styled-components";
 
-const StyledToggle = styled(IoIosArrowForward)`
+const StyledToggle = styled(IoIosArrowBack)`
   background-color: white;
   font-size: 1.5rem;
   box-shadow: 5px 0px 5px 0px rgba(0, 0, 0, 0.106);
 `;
 
-const StyledClosedToggle = styled(IoIosArrowBack)`
+const StyledClosedToggle = styled(IoIosArrowForward)`
   background-color: white;
   font-size: 1.5rem;
   box-shadow: 5px 0px 5px 0px rgba(0, 0, 0, 0.106);
