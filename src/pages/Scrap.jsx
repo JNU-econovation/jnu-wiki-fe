ff508a1b11a83788b11759bea3d9218495cd9b20
GimGayeon <kky1373@naver.com>
2023-09-12 10:49:53 +0900

    feat:mypage_scrab

--- src/pages/Scrap.jsx
+++ src/pages/Scrap.jsx
@@ -6,26 +6,7 @@ const Scrap = () => {
   return (
     <>
       <MainLayout myActive={true}>
-        {localStorage.getItem("token") ? (
-          <MypageScrap></MypageScrap>
-        ) : (
-          Swal.fire({
-            icon: "info",
-            title: "로그인 후 이용 가능합니다.",
-            text: "로그인 하시겠습니까?",
-            showCancelButton: true,
-            confirmButtonText: "예", // confirm 버튼 텍스트 지정
-            cancelButtonText: "아니오",
-            confirmButtonColor: "#429f50",
-            cancelButtonColor: "#d33",
-          }).then((result) => {
-            if (result.isConfirmed) {
-              location.href = routes.login;
-            } else if (result.isDismissed) {
-              location.href = routes.home;
-            }
-          })
-        )}
+        <MypageScrap />
       </MainLayout>
     </>
   );
