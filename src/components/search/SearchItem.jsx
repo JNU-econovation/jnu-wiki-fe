526aad9e9dabf24ae11406468de63fabfddeb5de
baegyeong <beth0929@naver.com>
2023-09-20 12:02:14 +0900

    fix: 커서 포인터로 지정

--- src/components/search/SearchItem.jsx
+++ src/components/search/SearchItem.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Title = styled.div`
   color: #216d32;
   margin-bottom: 2rem;
+  cursor: pointer;
 `;
 const SearchItem = ({ name, onClick }) => {
   return <Title onClick={onClick}>{name}</Title>;
