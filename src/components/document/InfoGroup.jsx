66f1f44171bb332e994fb7dc9083f966fc84ef78
baegyeong <beth0929@naver.com>
2023-09-26 21:02:23 +0900

    fix: container 삭제

--- src/components/document/InfoGroup.jsx
+++ src/components/document/InfoGroup.jsx
@@ -1,19 +1,11 @@
-import styled from "styled-components";
 import DocumentLabel from "./DocumentLabel";
 import InfoContent from "./InfoContent";
-
-const Container = styled.div`
-  display: block;
-  margin: 1.8rem 0;
-`;
 
 const InfoGroup = ({ htmlFor, children, label }) => {
   return (
     <>
-      <Container>
-        <DocumentLabel htmlFor={htmlFor}>{label}</DocumentLabel>
-        <InfoContent>{children}</InfoContent>
-      </Container>
+      <DocumentLabel htmlFor={htmlFor}>{label}</DocumentLabel>
+      <InfoContent>{children}</InfoContent>
     </>
   );
 };
