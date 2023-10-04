57060d372cb5621696e914a75231148556c09f6d
GimGayeon <kky1373@naver.com>
2023-10-04 12:00:06 +0900

    feat:mypage

--- src/services/user.jsx
+++ src/services/user.jsx
@@ -26,10 +26,7 @@ export const register = (data) => {
 
 export const onSilentRefresh = () => {
   instance
-    .post(
-      "/members/refresh-token"
-      //  { withCredentials: true }
-    )
+    .post("/members/refresh-token", { withCredentials: true })
     .then((response) => {
       alert("성공");
     })
@@ -52,19 +49,6 @@ export const nicknameDoubleCheck = (name) => {
 
 export const emailDBCheck = (email) => {
   return instance.post("members/check/email", { email });
-};
-
-///mypage
-export const getUserInfo = () => {
-  return instance.get("members/info");
-};
-
-export const getChangeInfo = (data) => {
-  const { Newnickname, Newpassword } = data;
-  return instance.post("members/modify/change", {
-    nickname: Newnickname,
-    password: Newpassword,
-  });
 };
 
 //admin List
