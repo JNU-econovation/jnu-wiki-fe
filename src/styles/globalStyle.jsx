c0787fb634ab5ce1d64ea86ff4c9d93d938fd9d7
baegyeong <beth0929@naver.com>
2023-10-11 19:59:00 +0900

    feat: 스크롤바 스타일

--- src/styles/globalStyle.jsx
+++ src/styles/globalStyle.jsx
@@ -1,9 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    html,body{
-        width:100vw;
-        font-family: var(--font-pretendard-light);
-        overflow:hidden;
+   html, body{
+    width: 100vw;
+    font-family: var(--font-pretendard-light);
+
+   &::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    border-radius: 6px;
+    background: rgba(237, 214, 214, 0.4);
+  }
+    &::-webkit-scrollbar-thumb {
+        background: rgba(86, 77, 77, 0.3);
+        border-radius: 6px;
     }
+ }
 `;
