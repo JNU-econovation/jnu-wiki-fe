d2fe441221a60304e1c31509bc2bad45a22a3c07
baegyeong <beth0929@naver.com>
2023-11-09 17:58:18 +0900

    fix: 지역 검색 수정

--- src/components/common/layout/OtherMap.jsx
+++ src/components/common/layout/OtherMap.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useCallback, useRef, useState } from "react";
+import { useEffect, useCallback, useRef, useState, memo } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import Button from "./Button";
@@ -53,9 +53,10 @@ const Btn = styled.div`
   right: 5%;
 `;
 
-const OtherMap = ({ title, apiLat, apiLng }) => {
+const OtherMap = memo(function OtherMap({ title, apiLat, apiLng }) {
   const dispatch = useDispatch();
-  const markerRef = useRef(null);
+  const container = useRef(null);
+
   const [showSearchButton, setShowSearchButton] = useState(false);
   const [swLatlng, setSwLatlng] = useState(0);
   const [neLatlng, setNeLatlng] = useState(0);
@@ -65,30 +66,11 @@ const OtherMap = ({ title, apiLat, apiLng }) => {
   }, []);
 
   let map, bounds;
-  let marker = new kakao.maps.Marker();
-  let geocoder = new kakao.maps.services.Geocoder();
 
-  const initialMap = () => {
-    const container = document.getElementById("map");
-    const options = {
-      center: new kakao.maps.LatLng(35.17614029042555, 126.90977266483199),
-      level: 4,
-    };
-    map = new kakao.maps.Map(container, options);
+  const options = {
+    center: new kakao.maps.LatLng(35.17614029042555, 126.90977266483199),
+    level: 4,
   };
-
-  const setSwNe = () => {
-    bounds = map.getBounds();
-    setSwLatlng(bounds.getSouthWest());
-    setNeLatlng(bounds.getNorthEast());
-  };
-
-  // const initSwNe = () => {
-  //   dispatch({
-  //     type: "getSwNe",
-  //     payload: { swLatlng, neLatlng },
-  //   });
-  // };
 
   const handleOnMap = () => {
     dispatch({
@@ -99,64 +81,15 @@ const OtherMap = ({ title, apiLat, apiLng }) => {
   };
 
   const mapscript = () => {
-    initialMap();
-    // setSwNe();
-    // initSwNe();
+    map = new kakao.maps.Map(container.current, options);
 
     kakao.maps.event.addListener(map, "bounds_changed", function () {
-      setSwNe();
-
       setShowSearchButton(true);
     });
 
-    function searchDetailAddrFromCoords(coords, callback) {
-      // 좌표로 법정동 상세 주소 정보를 요청
-      geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
-    }
-
-    let marker = new kakao.maps.Marker(),
-      infowindow = new kakao.maps.InfoWindow({ zindex: 1 });
-
-    kakao.maps.event.addListener(map, "click", function (mouseEvent) {
-      searchDetailAddrFromCoords(mouseEvent.latLng, function (result, status) {
-        let latposition = mouseEvent.latLng.getLat();
-        let lngposition = mouseEvent.latLng.getLng();
-
-        dispatch({
-          type: "getLatLng",
-          payload: { latitude: latposition, longitude: lngposition },
-        });
-
-        if (status === kakao.maps.services.Status.OK) {
-          let detailAddr = result[0].road_address
-            ? "<div>도로명주소 : " +
-              result[0].road_address.address_name +
-              "</div>"
-            : "";
-          detailAddr +=
-            "<div>지번 주소 : " + result[0].address.address_name + "</div>";
-
-          const payloadAddress = result[0].road_address
-            ? result[0].road_address.address_name
-            : result[0].address.address_name;
-
-          dispatch({
-            type: "getAddress",
-            payload: { address: payloadAddress },
-          });
-
-          let content = '<div class="bAddr">' + detailAddr + "</div>";
-
-          // 마커를 클릭한 위치에 표시
-          marker.setPosition(mouseEvent.latLng);
-          marker.setMap(map);
-
-          // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시
-          infowindow.setContent(content);
-          infowindow.open(map, marker);
-        }
-      });
-    });
+    bounds = map.getBounds();
+    setSwLatlng(bounds.getSouthWest());
+    setNeLatlng(bounds.getNorthEast());
 
     // 마커 여러개
     if (apiLat?.length > 0 && apiLng?.length > 0) {
@@ -193,45 +126,11 @@ const OtherMap = ({ title, apiLat, apiLng }) => {
         infowindow.close();
       };
     }
+    return map;
   };
 
-  const setAddress = useCallback(() => {
-    // 백엔드에서 보내준 좌표대로 주소 출력
-    let geocoder = new kakao.maps.services.Geocoder();
-    let coord = new kakao.maps.LatLng(apiLat, apiLng);
-
-    let callback = function (result, status) {
-      if (status === kakao.maps.services.Status.OK) {
-        const payloadAddress = result[0].road_address
-          ? result[0].road_address.address_name
-          : result[0].address.address_name;
-        dispatch({
-          type: "getAddress",
-          payload: { address: payloadAddress },
-        });
-        dispatch({
-          type: "initialAddress",
-          payload: { initialAddress: payloadAddress },
-        });
-      }
-    };
-
-    markerRef.current = new kakao.maps.Marker({
-      position: coord,
-    });
-
-    marker.setPosition(new kakao.maps.LatLng(apiLat, apiLng));
-    marker.setMap(map);
-
-    geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
-  }, [apiLat, apiLng, dispatch, map, marker]);
-
-  useEffect(() => {
-    setAddress();
-  }, [apiLat, apiLng, map, marker, setAddress]);
-
   return (
-    <>
+    <div className="container">
       {showSearchButton && (
         <Btn id="search">
           <Button backgroundcolor="primary" color="white" onClick={handleOnMap}>
@@ -241,13 +140,14 @@ const OtherMap = ({ title, apiLat, apiLng }) => {
       )}
       <MapDiv
         id="map"
+        ref={container}
         style={{
           width: "100vw",
           height: "100vh",
         }}
       />
-    </>
+    </div>
   );
-};
+});
 
 export default OtherMap;
