4d4df90d6c1daf9e395b167d97b8ff778b4fa54e
baegyeong <beth0929@naver.com>
2023-12-01 03:57:51 +0900

    style: 문서 전체 목록에 border radius 적용

--- src/components/document/DocsList.jsx
+++ src/components/document/DocsList.jsx
@@ -14,6 +14,7 @@ const Container = styled.div`
 
   background-color: white;
   box-shadow: 10px 0px 5px 0px rgba(0, 0, 0, 0.106);
+  border-radius: 0 0 10px 0;
 
   overflow-y: auto;
   max-height: calc(100vh - 6rem - 2 * 2rem);
