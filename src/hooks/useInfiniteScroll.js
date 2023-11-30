0ba1510513755c17477499b1c98e0dd2f509da9c
baegyeong <beth0929@naver.com>
2023-12-01 03:59:50 +0900

    feat: 이 위치 검색 기능 구현 및 문서 목록 조회 시 딜레이 적용
    - 무한스크롤 커스텀 훅 적용

---
+++ src/hooks/useInfiniteScroll.js
@@ -0,0 +1,51 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useRef, useEffect } from "react";
+
+const useInfiniteScroll = (queryKey, queryFn, ...parameters) => {
+  const bottomObserver = useRef(null);
+  parameters = parameters[0];
+
+  const { data, isLoading, isError, fetchNextPage, hasNextPage } =
+    useInfiniteQuery(
+      [queryKey, parameters],
+      ({ pageParam = 0 }) => queryFn({ pageParam, ...parameters }),
+      {
+        getNextPageParam: (lastPage, allPages) => {
+          const nextPage = allPages.length + 1;
+          return lastPage.currentPage < lastPage.totalPages
+            ? nextPage
+            : undefined;
+        },
+        refetchOnWindowFocus: true,
+      }
+    );
+
+  useEffect(() => {
+    const io = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && !isLoading && hasNextPage) {
+            fetchNextPage();
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    if (bottomObserver.current) {
+      io.observe(bottomObserver.current);
+    }
+
+    return () => {
+      if (bottomObserver.current) {
+        io.unobserve(bottomObserver.current);
+      }
+    };
+  }, [isLoading, hasNextPage, fetchNextPage]);
+
+  return { data, isLoading, isError, bottomObserver };
+};
+
+export default useInfiniteScroll;
