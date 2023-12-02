953805f32053e7f27b51e1eb5c129c4bffe7fd9c
baegyeong <beth0929@naver.com>
2023-12-02 16:21:08 +0900

    fix: 컴포넌트 스타일 상속을 위한 className 속성 추가

--- src/components/document/DocumentLabel.jsx
+++ src/components/document/DocumentLabel.jsx
@@ -2,16 +2,17 @@ import styled from "styled-components";
 
 const SpanWidth = styled.div`
   display: inline-block;
-  width: 4rem;
+  width: 4.5rem;
 `;
 
 const LabelStyled = styled.label`
   color: #216d32;
+  font-size: 1.1rem;
 `;
 
-const DocumentLabel = ({ htmlFor, children }) => {
+const DocumentLabel = ({ htmlFor, children, className }) => {
   return (
-    <SpanWidth>
+    <SpanWidth className={className}>
       <LabelStyled htmlFor={htmlFor}>{children}</LabelStyled>
     </SpanWidth>
   );
