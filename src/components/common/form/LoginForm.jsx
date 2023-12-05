bfaa809c5183260e009a89d3304534215a5e39dd
GimGayeon <kky1373@naver.com>
2023-12-06 00:18:57 +0900

    fix:로그인 시 reload

--- src/components/common/form/LoginForm.jsx
+++ src/components/common/form/LoginForm.jsx
@@ -79,6 +79,7 @@ const LoginForm = () => {
             loginSuccessAlert().then((result) => {
               if (result.isConfirmed) {
                 navigate(routes.home);
+                location.reload();
               }
             });
           })
