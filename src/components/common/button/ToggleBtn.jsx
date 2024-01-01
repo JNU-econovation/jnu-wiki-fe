3561ecb09ae3a1a21dd637d124b6469ee8fdd206
baegyeong <beth0929@naver.com>
2024-01-01 17:38:58 +0900

    refactor: 불필요한 컴포넌트 삭제

--- src/components/common/button/ToggleBtn.jsx
+++ src/components/common/button/ToggleBtn.jsx
@@ -4,27 +4,21 @@ import styled, { css } from "styled-components";
 const ToggleBtn = ({ toggle, onClick }) => {
   return (
     <div onClick={onClick}>
-      {toggle ? (
-        <OpenToggleBtn>
-          <StyledToggle />
-        </OpenToggleBtn>
-      ) : (
-        <ClosedToggleBtn>
-          <StyledClosedToggle />
-        </ClosedToggleBtn>
-      )}
+      {toggle ? <StyledToggle /> : <StyledClosedToggle />}
     </div>
   );
 };
 
 const baseStyles = css`
   cursor: pointer;
+
   &:hover {
     font-size: 2.2rem;
     transition-duration: 100ms;
   }
 
   background-color: white;
+
   font-size: 1.8rem;
   border-radius: 0 5px 5px 0;
   box-shadow: 5px 0px 5px 0px rgba(0, 0, 0, 0.106);
@@ -32,19 +26,15 @@ const baseStyles = css`
 
 const StyledToggle = styled(IoIosArrowBack)`
   ${baseStyles}
-`;
 
-const StyledClosedToggle = styled(IoIosArrowForward)`
-  ${baseStyles}
-`;
-
-const OpenToggleBtn = styled.div`
   position: absolute;
   left: calc(15rem + 27rem);
   top: calc((100vh - 6rem) / 2);
 `;
 
-const ClosedToggleBtn = styled.div`
+const StyledClosedToggle = styled(IoIosArrowForward)`
+  ${baseStyles}
+
   position: absolute;
   left: 15rem;
   top: calc((100vh - 6rem) / 2);
