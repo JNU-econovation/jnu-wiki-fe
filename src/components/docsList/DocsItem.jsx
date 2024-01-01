099be139e66754f73ad85b9830da243a86288b0f
baegyeong <beth0929@naver.com>
2024-01-01 23:28:57 +0900

    refactor: 문서 개별 아이템 스타일 코드 통합

--- src/components/docsList/DocsItem.jsx
+++ src/components/docsList/DocsItem.jsx
@@ -1,6 +1,6 @@
-import styled from "styled-components";
 import { useState } from "react";
 import ScrapBtn from "@/components/common/button/ScrapBtn";
+import { Container, StyledHr } from "@/styles/DocsItem";
 
 const DocsItem = ({
   name,
@@ -37,37 +37,5 @@ const DocsItem = ({
     </>
   );
 };
-
-const Container = styled.div`
-  color: #216d32;
-
-  display: flex;
-  flex-direction: column;
-
-  min-width: 15rem;
-  max-width: 22rem;
-
-  cursor: pointer;
-
-  .title {
-    font-size: 1.1rem;
-    font-weight: bold;
-
-    margin-right: 0.8rem;
-    margin-bottom: 1.3rem;
-
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .category {
-    font-size: 0.8rem;
-  }
-`;
-
-const StyledHr = styled.hr`
-  background-color: #8ea192;
-  margin: 1.4rem 0;
-`;
 
 export default DocsItem;
---
+++ src/styles/DocsItem.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  color: #216d32;
+  font-size: 1.2rem;
+
+  display: flex;
+  flex-direction: column;
+
+  width: 16rem;
+
+  cursor: pointer;
+
+  .title {
+    font-size: 1.2rem;
+    font-weight: bold;
+
+    margin-bottom: 0.9rem;
+
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .category {
+    font-size: 0.9rem;
+  }
+`;
+
+export const StyledHr = styled.hr`
+  background-color: #c7c7c6;
+  border: 0;
+  width: 100%;
+  height: 1px;
+  margin: 1.5rem 0;
+`;
