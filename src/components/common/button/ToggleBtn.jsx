be85c2b8af049e0efc94a5af5ea27afb05ecddc0
baegyeong <beth0929@naver.com>
2024-01-01 17:17:24 +0900

    styled: 토글 버튼 스타일 코드 중복 삭제 및 hover 지정

--- src/components/common/button/ToggleBtn.jsx
+++ src/components/common/button/ToggleBtn.jsx
@@ -1,21 +1,46 @@
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const StyledToggle = styled(IoIosArrowBack)`
+const ToggleBtn = ({ toggle, onClick }) => {
+  return (
+    <div onClick={onClick}>
+      {toggle ? (
+        <OpenToggleBtn>
+          <StyledToggle />
+        </OpenToggleBtn>
+      ) : (
+        <ClosedToggleBtn>
+          <StyledClosedToggle />
+        </ClosedToggleBtn>
+      )}
+    </div>
+  );
+};
+
+const baseStyles = css`
+  cursor: pointer;
+  &:hover {
+    font-size: 2.2rem;
+    transition-duration: 100ms;
+  }
+
   background-color: white;
-  font-size: 1.5rem;
+  font-size: 1.8rem;
+  border-radius: 0 5px 5px 0;
   box-shadow: 5px 0px 5px 0px rgba(0, 0, 0, 0.106);
 `;
 
+const StyledToggle = styled(IoIosArrowBack)`
+  ${baseStyles}
+`;
+
 const StyledClosedToggle = styled(IoIosArrowForward)`
-  background-color: white;
-  font-size: 1.5rem;
-  box-shadow: 5px 0px 5px 0px rgba(0, 0, 0, 0.106);
+  ${baseStyles}
 `;
 
 const OpenToggleBtn = styled.div`
   position: absolute;
-  left: 37rem;
+  left: calc(15rem + 27rem);
   top: calc((100vh - 6rem) / 2);
 `;
 
@@ -24,21 +49,5 @@ const ClosedToggleBtn = styled.div`
   left: 15rem;
   top: calc((100vh - 6rem) / 2);
 `;
-
-const ToggleBtn = ({ toggle, onClick }) => {
-  return (
-    <>
-      {toggle ? (
-        <OpenToggleBtn>
-          <StyledToggle onClick={onClick} />
-        </OpenToggleBtn>
-      ) : (
-        <ClosedToggleBtn>
-          <StyledClosedToggle onClick={onClick} />
-        </ClosedToggleBtn>
-      )}
-    </>
-  );
-};
 
 export default ToggleBtn;
