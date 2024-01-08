1ffa83d1206c4407806fdaa30ccf270a12061194
baegyeong <beth0929@naver.com>
2024-01-09 00:45:04 +0900

    style: 회원가입 폼 반응형 구현

--- src/components/register/Container.jsx
+++ src/components/register/Container.jsx
@@ -12,7 +12,7 @@ const Container = ({ children, marginBottom, onSubmit }) => {
       marginBottom={marginBottom}
       onSubmit={onSubmit}
     >
-      {children}
+      <div>{children}</div>
     </ContainerCss>
   );
 };
@@ -25,13 +25,35 @@ const ContainerCss = styled.form`
   box-shadow: 0px 4px 64px rgba(0, 0, 0, 0.05);
 
   width: 26rem;
-  padding: 0 3rem 0 3rem;
+  padding: 4rem 4rem 3rem 4rem;
 
   position: relative;
   left: 37%;
   top: 15rem;
   z-index: 1;
   margin-bottom: 30rem;
+  box-sizing: content-box;
+
+  @media screen and (max-width: 1023px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    top: 8rem;
+    left: auto;
+
+    margin: 0 auto;
+    padding: 0;
+
+    box-shadow: initial;
+    border: initial;
+    margin-bottom: 0;
+  }
+
+  @media screen and (max-width: 767px) {
+    width: 100%;
+  }
 `;
 
 export default Container;
