dc424c68999221ff86510948d806d0517fe8aea9
baegyeong <beth0929@naver.com>
2024-05-06 00:07:02 +0900

    style: 검색바 반응형일 때 위치 변경

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -49,12 +49,13 @@ const SearchBar = ({ isDisplay }) => {
 };
 
 const Container = styled.section`
+  position: relative;
   width: 40rem;
   display: ${(props) => props.isDisplay === false && "none"};
 
   > * {
     border: 0.5px solid #71717118;
-    border-radius: 10px;
+    border-radius: 8px;
     width: 100%;
   }
 
@@ -62,12 +63,8 @@ const Container = styled.section`
     width: 45%;
   }
 
-  @media screen and (max-width: 767px) {
-    position: absolute;
-    top: 5rem;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 80%;
+  @media screen and (max-width: 1272px) {
+    width: 50%;
   }
 `;
 
@@ -87,6 +84,11 @@ const StyledSearchBar = styled.input`
     background-repeat: no-repeat;
     text-indent: 0;
   }
+
+  @media screen and (max-width: 767px) {
+    box-shadow: none;
+    border: 0;
+  }
 `;
 
 const StyledSearchResult = styled.article`
@@ -97,6 +99,8 @@ const StyledSearchResult = styled.article`
   padding: 3rem 1.3rem;
 
   background-color: white;
+  box-shadow: 0px 3px 13px -4px rgba(0, 0, 0, 0.206);
+
   box-sizing: border-box;
   overflow-y: scroll;
 
@@ -106,8 +110,8 @@ const StyledSearchResult = styled.article`
 `;
 
 const StyledLoader = styled(Loader)`
+  position: absolute;
   transform: scale(0.8);
-  margin-left: -3rem;
 `;
 
 export default SearchBar;
