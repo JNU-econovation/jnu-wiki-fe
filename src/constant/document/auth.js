c5babe1426b6b7a66f0eadcc6ba748ce2242a0ef
baegyeong <beth0929@naver.com>
2024-05-18 02:06:37 +0900

    fix: 안내 문구 변경

--- src/constant/document/auth.js
+++ src/constant/document/auth.js
@@ -1,11 +1,11 @@
 export const JOIN_DOUBLE_CHECK = Object.freeze({
   FAILED: {
-    EMAIL: "이미 존재하는 이메일입니다.",
-    NICKNAME: "이미 존재하는 닉네임입니다.",
+    EMAIL: "이미 존재하는 이메일이에요.",
+    NICKNAME: "이미 존재하는 닉네임이에요.",
   },
   SUCCESS: {
-    EMAIL: "사용 가능한 이메일입니다.",
-    NICKNAME: "사용 가능한 닉네임입니다.",
+    EMAIL: "사용 가능한 이메일이에요.",
+    NICKNAME: "사용 가능한 닉네임이에요.",
   },
   DOUBLED: {
     EMAIL: "이메일 중복 확인을 해주세요.",
